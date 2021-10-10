9c1e39dadd4823d01efb285dfd12b9fb5617e755
blooper-ser <89916095+blooper-ser@users.noreply.github.com>
2021-10-10 13:50:43 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -139,7 +139,7 @@ const array = ['ya','apo','bomb','ee','list','machu','menu','mute','nee','onn','
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio , mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
     }
