bb1565a1f175a0ae576d25ed9763774747d510c2
blooper-ser <89916095+blooper-ser@users.noreply.github.com>
2021-10-10 14:30:32 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,21 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[1] = "https://i.imgur.com/8D0kBWf.jpg";
+  r_text[0] = "https://i.imgur.com/8D0kBWf.jpg";
     
     
-    var i = Math.floor(0*Math.random())
+    var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Bemin*
-*owner number wa.me/919744267478*
-
-*My Channel : https://www.youtube.com/channel/UC56baqlzh4BQET7S1jASiFA*
-
-*github link : https://github.com/blooper-ser/blooper*
-
-*audio commads : https://github.com/blooper-ser/blooper/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
+*Creator number : wa.me/919744267478?text=Hi%20Blooper%20Ser.%20*
+*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+*Githublink (Setup)  :    https://github.com/blooper-ser/blooper*
+*Audio commads :   https://github.com/blooper-ser/blooper/tree/master/uploads*
+*Sticker commads : https://github.com/blooper-ser/blooper/tree/master/stickers*
 `}) 
 
 }));
